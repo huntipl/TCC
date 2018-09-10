37814f70cd1bed1b12b3260421a482ba5d203c7f
Peter <3rd@petermartyniak.com>
2018-09-10 02:09:50 +0100

    added test checking for 0 chunks split

--- tcc.py
+++ tcc.py
@@ -1,2 +1,4 @@
 def split_array(aray, n):
+    if n == 0:
+        raise ValueError("n has to be greater than 0")
     return [[]]
--- test/test_tcc.py
+++ test/test_tcc.py
@@ -1,7 +1,14 @@
 import tcc
+import pytest
 
 
-def test_empty_array_returns_empty_array():
-    result = tcc.split_array([],1)
+@pytest.mark.parametrize("test_chunks",[1,3,5])
+def test_empty_array_split_returns_empty_array(test_chunks):
+    result = tcc.split_array([],test_chunks)
     expected = [[]]
     assert expected == result
+
+
+def test_split_to_0_chunks_raises_valueerror_exception():
+    with pytest.raises(ValueError):
+        tcc.split_array([],0)
