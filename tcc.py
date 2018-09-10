65009a0aef7d95fa91ae99e68d85693a37d4b007
Peter <3rd@petermartyniak.com>
2018-09-10 02:51:11 +0100

    refactored the code

--- tcc.py
+++ tcc.py
@@ -2,20 +2,29 @@ import collections
 import math
 
 
-def split_array(array, n):
-    """"Returns the input array split into n chunks"""
+def sanitize_input(func):
+    """Checks for array being type collection and positive integer n"""
 
-    if not isinstance(array, collections.Sequence):
-        raise ValueError("array has to be array or list")
-    if not isinstance(n, int):
-        raise ValueError("n has to be an integer")
-    if n < 1:
-        raise ValueError("n has to be greater than 0")
+    def func_wrapper(array, n):
+        if not isinstance(array, collections.Sequence):
+            raise ValueError("array has to be array or list")
+        if not isinstance(n, int):
+            raise ValueError("n has to be an integer")
+        if n < 1:
+            raise ValueError("n has to be greater than 0")
+        return func(array, n)
+    return func_wrapper
+
+
+@sanitize_input
+def split_array(array, n):
+    """"Returns the input array split into n chunks
+    :type array: collections.Sequence
+    :type n: int
+    """
 
     array_size = len(array)
     max_chunk_size = int(math.ceil(array_size/n))
-
-    print(array_size, max_chunk_size)
 
     if array_size == 0:
         return [[]]
